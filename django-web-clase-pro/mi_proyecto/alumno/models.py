2b45e169b815ca45e493e5cb4fe8fa0998d6f5ce
Rodavlas7 <shavitagarciab@gmail.com>
2026-03-03 14:37:09 -0800

    modificacion alumno y creacion de curso

--- django-web-clase-pro/mi_proyecto/alumno/models.py
+++ django-web-clase-pro/mi_proyecto/alumno/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Alumno(models.Model):
+    nombre = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=100)
+    email = models.EmailField()
+    matricula = models.CharField(max_length=20, unique=True)
+
+    def __str__(self):
+        return f"{self.nombre} {self.apellido} ({self.matricula})"
