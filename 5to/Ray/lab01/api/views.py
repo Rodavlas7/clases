790cdf57b9bf40c7616864b67b5b3f81ae88baed
Rodavlas7 <shavitagarciab@gmail.com>
2026-05-20 08:52:28 -0700

    actividad de ray

---
+++ 5to/Ray/lab01/api/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from rest_framework import generics
+from django.contrib.auth.models import User
+from api.serializers import UserSerializer
+
+# Create your views here.
+
+class UserListView(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
