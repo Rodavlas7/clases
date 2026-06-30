276fe0c2811c0bfebae9ab0e87703de437320abf
Rodavlas7 <shavitagarciab@gmail.com>
2026-06-30 07:12:29 -0700

    my little advance

--- 5to/Ray/client/home/views.py
+++ 5to/Ray/client/home/views.py
@@ -5,22 +5,17 @@ from home import forms
 # Create your views here.
 
 class Index(generic.View):
-    template_name = "home/index.html"
-    url_base = "http://127.0.0.1:8001/api/v1/users/list" 
+    template_name = 'home/list_banks.html'
+    context = {}
+    url_base = "http://127.0.0.1:8001/api/v1/banks/list"
+    response = None
 
     def get(self, request):
-        try:
-            res = requests.get(self.url_base)
-            res.raise_for_status() 
-            users_data = res.json()
-        except Exception as e:
-            print(f"--- ERROR EN API USUARIOS: {e} ---")
-            users_data = [] 
-
-        context = {
-            "users": users_data
+        self.response = requests.get(url=self.url_base).json()
+        self.context = {
+            "banks" : self.response
         }
-        return render(request, self.template_name, context)
+        return render(request, self.template_name, self.context)
     
 
 ### BANKS VIEWS
